5532435df7801cc99cf0fcdd8f293ed9dd2b96f0
MattWis <matthew.wismer@students.olin.edu>
2013-08-23 22:16:27 -0400

    Moved num_ocr.cpp to scripts/

--- scripts/num_ocr.cpp
+++ scripts/num_ocr.cpp
@@ -10,7 +10,7 @@ cv::Vec3b blackOrWhite (bool white) {
 }
 
 bool isRed(cv::Vec3b color) {
-  return (color[2] > 200 && (color[0] + color[1] < 120));
+  return (color[2] > 200 && (abs(color[0] - color[1]) < 20));
 }
 
 bool isWhite(cv::Vec3b color) {
@@ -30,28 +30,31 @@ void smartGrayscale(cv::Mat& src, cv::Mat dst) {
   }
 }
 
-char* readNumFromImage(cv::Mat image) {
+int readNumFromImage(cv::Mat image) {
   tesseract::TessBaseAPI tess;
   tess.Init(NULL, "eng", tesseract::OEM_DEFAULT);
   tess.SetVariable("tessedit_char_whitelist", "0123456789");
   tess.SetPageSegMode(tesseract::PSM_SINGLE_BLOCK);
   tess.SetImage((uchar*)image.data, image.cols, image.rows, 1, image.cols);
 
-  return tess.GetUTF8Text();
+  int image_num = -1;
+
+  char *imageText = tess.GetUTF8Text();
+  if (imageText != "")
+    sscanf(imageText, "%d", &image_num);
+
+  return image_num;
 }
 
-void getPercentageFromImage(const char *filename) {
-  std::string x = filename;
-  std::cout << filename << std::endl;
-
+int getPercentageFromImage(std::string filename) {
   cv::Mat full_color = cv::imread(filename);
-  if (!full_color.data)
-    return;
+  if (!full_color.data) {
+    fprintf(stderr, "File not found\n");
+    return -1;
+  }
 
   cv::Mat gray = full_color.clone();
   smartGrayscale(full_color, gray);
-
-  cv::imwrite("preprocessed" + x, gray);
 
   cv::Mat prefiltered;
   cv::cvtColor(gray, prefiltered, CV_BGR2GRAY);
@@ -60,27 +63,27 @@ void getPercentageFromImage(const char *filename) {
 
   std::vector<std::vector<cv::Point> > contours;
   cv::findContours(noise.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-  for (int i = 0; i < contours.size(); i++)
-  {
+
+  for (int i = 0; i < contours.size(); i++) {
     if(cv::contourArea(contours[i]) > 20)
       cv::drawContours(noise, contours, i, cv::Scalar(0), -1);
   }
 
   filtered = prefiltered & (noise < 100);
 
-  cv::imwrite("prefiltered" + x, prefiltered);
-  cv::imwrite("noise" + x, noise);
-  cv::imwrite("not_noise" + x, (noise < 100));
-  cv::imwrite("filtered" + x, filtered);
-
-  char* out = readNumFromImage(filtered);
-  std::cout << out << std::endl;
+  return readNumFromImage(filtered);
 }
 
 int main() {
-  getPercentageFromImage("00100.tiff");
-  std::cout << std::endl;
-  getPercentageFromImage("00300.tiff");
+  std::string filename;
+  getline(std::cin, filename);
+  int percent;
+
+  while (filename != "") {
+    percent = getPercentageFromImage(filename);
+    std::cout << filename << ", " << percent << std::endl;
+    getline(std::cin, filename);
+  }
 
   return 0;
 }
